f9001e1eb610a6a9b1fe81ded27e6e9679400663
Aghogho Omoware <a_omoware@hotmail.com>
2020-05-30 18:39:52 +0200

    debugging

--- shop/customers/routes.py
+++ shop/customers/routes.py
@@ -68,50 +68,46 @@ def get_order():
     if current_user.is_authenticated:
         customer_id = current_user.id
         invoice = secrets.token_hex(5)
-        try:
-            mollie_client = Client()
-            mollie_client.set_api_key(os.environ.get('API_KEY'))
-            amount = cal_cart_total(session['Shoppingcart'])
-            
-            payment = mollie_client.payments.create({
-                'amount': {
-                    'currency': 'EUR',
-                    'value': str(amount)
-                },
-                'description': 'Payment for invoice: ' + invoice,
-                'redirectUrl': 'http://verclean-531794983.eu-west-1.elb.amazonaws.com/ordercomplete/' + invoice,
-                'webhookUrl': 'https://verclean-531794983.eu-west-1.elb.amazonaws.com/mollie-webhook/',
-                'metadata': {
-                    'invoice': str(invoice)
-                }
-            })
         
-            if payment.status == 'open':
-                payments = Payments(
-                    status = payment.status,
-                    amount = amount,
-                    invoice = invoice,
-                    payment_id = payment.id
-                )
+        mollie_client = Client()
+        mollie_client.set_api_key(os.environ.get('API_KEY'))
+        amount = cal_cart_total(session['Shoppingcart'])
             
-                order = CustomerOrder(
-                    invoice = invoice,
-                    customer_id = customer_id,
-                    orders = session['Shoppingcart']
-                )
-                
-                db.session.add(payments)
-                db.session.add(order)
-                db.session.commit()
-                
-                session.pop('Shoppingcart')
-                return redirect(payment.checkout_url)
-            return redirect(url_for('carts'))
-                    
-        except Exception as e:
-            print(e)
-            flash('Something went wrong while getting orders', 'danger')
-            return redirect(url_for('get_carts'))
+        payment = mollie_client.payments.create({
+            'amount': {
+                'currency': 'EUR',
+                'value': str(amount)
+            },
+            'description': 'Payment for invoice: ' + invoice,
+            'redirectUrl': 'http://verclean-531794983.eu-west-1.elb.amazonaws.com/ordercomplete/' + invoice,
+            'webhookUrl': 'https://verclean-531794983.eu-west-1.elb.amazonaws.com/mollie-webhook/',
+            'metadata': {
+                'invoice': str(invoice)
+            }
+        })
+        
+        if payment.status == 'open':
+            payments = Payments(
+                status = payment.status,
+                amount = amount,
+                invoice = invoice,
+                payment_id = payment.id
+            )
+        
+            order = CustomerOrder(
+                invoice = invoice,
+                customer_id = customer_id,
+                orders = session['Shoppingcart']
+            )
+            
+            db.session.add(payments)
+            db.session.add(order)
+            db.session.commit()
+            
+            session.pop('Shoppingcart')
+        return redirect(payment.checkout_url)
+    return redirect(url_for('carts'))
+
 
 @app.route('/orders/<invoice>')
 @login_required
